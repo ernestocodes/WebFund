42c1d987d7414cdfedeb7855e14a14af1030259f
ErnestoCarballo <ernestowcarballo12@gmail.com>
2022-02-02 08:32:03 -0800

    second commit

--- Likes_P2/script.js
+++ Likes_P2/script.js
@@ -1,7 +1,10 @@
-var likes = 3;
-var likesElement = document.querySelector("#likes");
+var likes = [9,12,9];
+var likesElement = [
+    document.querySelector("#likes1"),
+    document.querySelector("#likes2"),
+    document.querySelector("#likes3")]
 
-function liked(){
-    likes++;
-    likesElement.innerText = likes + " like(s)";
+function liked(id){
+    likes[id]++;
+    likesElement[id].innerHTML = likes[id] + " like(s)";
 }
